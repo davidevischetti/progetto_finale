9f87053817b1c1590eca840fb7c38e3674257588
davide <davidevischetti@gmail.com>
2022-09-09 17:38:58 +0200

    order_table

---
+++ database/migrations/2022_09_09_153132_create_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('email',255)->unique();
+            $table->string('address',255)->unique();
+            $table->string('name',255);
+            $table->string('phone',20)->unique();
+            $table->smallInteger('price_total');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
