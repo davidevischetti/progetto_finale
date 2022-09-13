5598b495714a49feef0463e2329dc6bc0499d919
davide <davidevischetti@gmail.com>
2022-09-13 20:15:33 +0200

    layout_register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,16 +5,16 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+                <h4 class="card-header text-center">{{ __('Register') }}</h4>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
+                <div class="card-body row justify-content-center">
+                    <form class="row justify-content-center" method="POST" action="{{ route('register') }}">
                         @csrf
 
-                        <div class="form-group row">
+                        <div class="form-group row mb-4">
                             <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}*</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-8">
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}"  maxlength="255" required autocomplete="name" autofocus>
 
                                 @error('name')
@@ -25,10 +25,10 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
+                        <div class="form-group row mb-4">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}*</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-8">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
 
                                 @error('email')
@@ -39,10 +39,10 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
+                        <div class="form-group row mb-4">
                             <label for="address" class="col-md-4 col-form-label text-md-right">{{ __('Address') }}*</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-8">
                                 <input id="address" type="text" class="form-control @error('address') is-invalid @enderror" name="address" value="{{ old('address') }}"  maxlength="255" minlength="8" required autocomplete="address" autofocus>
 
                                 @error('address')
@@ -53,10 +53,10 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
+                        <div class="form-group row mb-4">
                             <label for="p_iva" class="col-md-4 col-form-label text-md-right">{{ __('Partita IVA') }}*</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-8">
                                 <input id="p_iva" type="number" class="form-control @error('p_iva') is-invalid @enderror" name="p_iva" value="{{ old('p_iva') }}" required autofocus min="1">
                                 @error('p_iva')
                                     <span class="invalid-feedback" role="alert">
@@ -67,10 +67,10 @@
                         </div>
 
                         {{-- IMMAGINE NULLABLE DA INSERIRE/ DEFAULT CATEGORIA --}}
-                        {{-- <div class="form-group row">
+                        {{-- <div class="form-group row mb-4">
                             <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-8">
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
                                 @error('name')
@@ -81,10 +81,10 @@
                             </div>
                         </div> --}}
 
-                        <div class="form-group row">
+                        <div class="form-group row mb-4">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}*</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-8">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" minlength="8">
 
                                 @error('password')
@@ -95,32 +95,24 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
+                        <div class="form-group row mb-4">
                             <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}*</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-8">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-
-
-
-
-                        <fieldset class="mb-3">
-                            <legend>categories*</legend>
-                            <div class="checkbox-group">
+                        {{-- SELEZIONE DELLA CATEGORIA --}}
+                        <fieldset class="mb-3 form-group row">
+                            <label for="check-category" class="col-md-4 col-form-label text-md-right">{{ __('Categories') }}*</label>
+                            <div class="checkbox-group col-md-6 row" id="check-category">
                                     @foreach ($categories as $category)
-                                    <input type="checkbox" class="form-check-input remove-required" name="categories[]" value="{{$category->id}}" id="category-{{$category->id}}" required
+                                    <div class="col-md-4 mb-2">
+                                        <input type="checkbox" class="form-check-input remove-required me-1" name="categories[]" value="{{$category->id}}" id="category-{{$category->id}}" required
                                         @if(in_array($category->id, old('categories') ?: [])) checked @endif>
-                                    <label for="category-{{$category->id}}" class="fomr-check-label">{{$category->name}}</label>
+                                        <label for="category-{{$category->id}}" class="fomr-check-label">{{$category->name}}</label>
+                                    </div>
                                     @endforeach
                             </div>
                             @error('categories')
@@ -130,8 +122,13 @@
                             @enderror
                         </fieldset>
 
-
-
+                        <div class="form-group row mt-5 mb-0">
+                            <div class="col-md-2 offset-md-5">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Register') }}
+                                </button>
+                            </div>
+                        </div>
 
                     </form>
                 </div>
