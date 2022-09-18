8540a308f233baf6e0455f328f6a3f13d1eca83b
AlessandroFulco <alessandro.fulco99@gmail.com>
2022-09-18 11:11:57 +0200

    add_paymentMethods_axios

--- app/Http/Controllers/Api/Orders/OrderController.php
+++ app/Http/Controllers/Api/Orders/OrderController.php
@@ -5,20 +5,44 @@ namespace App\Http\Controllers\Api\Orders;
 use Braintree\Gateway;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Orders\OrderRequest;
 
 class OrderController extends Controller
 {
     public function generate(Request $request, Gateway $gateway) {
         $token = $gateway->clientToken()->generate();
-        return response()->json([
+
+        $data = [
             'success' => true,
             'token' => $token
-        ]);
+        ];
+
+        return response()->json($data, 200);
     }
 
-    public function makePayment(Request $request, Gateway $gateway) {
-        return response()->json([
-            'success' => true,
+    public function makePayment(OrderRequest $request, Gateway $gateway) {
+
+        $result = $gateway->transaction()->sale([
+            'amount' => $request->amount,
+            'paymentMethodNonce' => $request->token,
+            'options' => [
+                'submitForSettlement' => true,
+            ]
         ]);
+
+        if($result->success) {
+            $data = [
+                'success' => true,
+                'message' => 'Transazione avvenuta con successo'
+            ];
+            return response()->json($data, 200);
+
+        } else {
+            $data = [
+                'success' => false,
+                'message' => 'Transazione Fallita'
+            ];
+            return response()->json($data, 401);
+        }
     }
 }
