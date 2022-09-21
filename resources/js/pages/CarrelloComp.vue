60386e131bf17813fde7c200288e8ce4ec41eabf
GiulsV <valente.giulia93@gmail.com>
2022-09-21 23:02:55 +0200

    creazione component Carrello

---
+++ resources/js/pages/CarrelloComp.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="myCart">
+
+    </div>
+</template>
+
+<script>
+export default{
+    name: 'CarrelloComp',
+    components:{
+
+    }
+}
+</script>
+
+<style lang="less" scoped>
+    #myCart{
+        background-color: #ffe6d8;
+    }
+</style>
