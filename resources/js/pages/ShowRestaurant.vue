825dc9602278cff3728f2020bb2df52a6e3204f9
GiulsV <valente.giulia93@gmail.com>
2022-09-21 19:47:57 +0200

    Modifica parentesi quadre array

--- resources/js/pages/ShowRestaurant.vue
+++ resources/js/pages/ShowRestaurant.vue
@@ -57,8 +57,8 @@ export default {
     data() {
         return {
             // idRistorante: id,
-            arrRestInfo: null,
-            arrRestPlate: null,
+            arrRestInfo: [],
+            arrRestPlate: [],
         }
     },
     created(){
