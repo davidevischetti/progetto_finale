19d27e8c66cead04ed3d97c9f5ca0456978f8cc8
MatteoPellegrinii <pellegrini.matteo00@gmail.com>
2022-09-26 17:09:03 +0200

    modifiche finali

--- resources/views/restaurant/dashboard.blade.php
+++ resources/views/restaurant/dashboard.blade.php
@@ -7,7 +7,7 @@
     <div class="row justify-content-start">
 
         {{-- TODO: aggiungere funzione per mandare il messaggio di avvenuto login/register  --}}
-            <div class="col-8 offset-2">
+            <div class="col-10 offset-2">
                 @if (session('registrato'))
                 <div class="alert alert-light alert-dismissible mt-2 fade show" role="alert">
                     Ciao {{$userName->name}},
@@ -23,23 +23,45 @@
                     </div>
                 @endif
             </div>
-
-        <h1 class="mb-4 text-center">
-            Menu
-        </h1>
-
-        <div class="col-2">
-            @include('restaurant/partials/side-bar')
+        
+        <div class="col-3 col-lg-2">
+                @include('restaurant/partials/side-bar')
         </div>
+        
+        <div class="col-3 mb-4 d-none d-lg-flex align-items-center justify-content-center ">
+            <img class="img_rest  w-75" src="{{"storage/".$userName->img}}" alt="">
+        </div>    
+        <div class="mb-4 col-5 d-flex flex-wrap align-items-center col-lg-3">
+            <h1 class="underline title_rest text-uppercase col-12 d-flex align-items-center justify-content-center">{{$userName->name}}</h1>
+        </div>
+        <div class="col-3 d-flex flex-wrap justify-content-center mb-4">
+            <h5 class="col-12">
+                Email: {{$userName->email}} 
+            </h5>
+            <h5 class="col-12">
+                Indirizzo: {{$userName->address}} 
+            </h5>
+            <h5 class="col-12">
+                P. iva: {{$userName->p_iva}} 
+            </h5>
+        </div>        
 
-        <div class="col-8">
+
+
+        <div class="col-9 offset-2">
+
+            <h2 class="text-uppercase border_rest text-center">IL TUO MENù</h2>
             <ul class="nav myBorder-primary flex-column list-group mb-2">
+                @if (count($platesVisible) == 0 )
+                <h1 class="message">Non hai ancora inserito dei piatti nel tuo menù</h1>
+                <div class="white border_matteo"></div>
+                @endif
                 @foreach ($platesVisible as $plate)
-                    <li  data-id="{{ $plate->id }}" class="nav-item d-flex align-items-center py-3 list-group-item" >
+                    <li  data-id="{{ $plate->id }}" class="nav-item border_matteo d-flex align-items-center py-3 list-group-item" >
                         {{-- <img src="{{ asset ('storage/' . $plate->img)}}" alt="" > --}}
                         <div class="col-8 my-0 align-items-center">
                             <h3  href="#">{{$plate->name}}</h3>
-                            <span>
+                            <span> 
                                 {{$plate->ingredients}}
                             </span>
                             <div class="mt-2">
--- resources/views/restaurant/partials/side-bar.blade.php
+++ resources/views/restaurant/partials/side-bar.blade.php
@@ -1,5 +1,5 @@
-<ul class="nav flex-column list-group myBorder-primary">
-    <li class="nav-item rounded-top">
+<ul class="nav flex-column list-group border_matteo myBorder-primary">
+    <li class="nav-item ">
       <a class="nav-link list-group-item" href="{{route('restaurant.dashboard')}}">Menu</a>
     </li>
     <li class="nav-item">
@@ -7,11 +7,5 @@
     </li>
     <li class="nav-item ">
       <a class="nav-link list-group-item" href="{{route('restaurant.plates.index')}}">Plates list</a>
-    </li>
-    <li class="nav-item ">
-      <a class="nav-link list-group-item" href="#">My orders</a>
-    </li>
-    <li class="nav-item rounded-bottom">
-      <a class="nav-link list-group-item" href="#">Orders stats</a>
     </li>
 </ul>
